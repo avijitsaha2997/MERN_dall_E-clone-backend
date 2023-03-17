89abf384d2a2ebdf53c650dad6abd884e2869ac1
Avijit Saha <jit.avi.saha2997@gmail.com>
2023-03-18 00:47:55 +0600

    initialize server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// hello avijit
+import express from "express"
+// import mongoose from "mongoose"
+import Cors from "cors"
+import * as dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+const port = 8001;
+
+app.use(express());
+app.use(express.json({ limit: "50mb" }))
+app.use(Cors());
+
+
+app.get("/", async (req, res) => {
+    res.send("Hello Avijit")
+});
+
+app.listen(port, () => console.log(`App is listening: ${port}`))
